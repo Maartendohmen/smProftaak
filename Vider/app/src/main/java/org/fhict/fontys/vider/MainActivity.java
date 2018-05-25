a2f742fce3bf39a6d69f6ee8b04decfc7a0be8c2
NickvanderHeijden <nickvdh44@gmail.com>
2018-05-25 10:52:13 +0200

    working on UI actionbar

--- Vider/app/src/main/java/org/fhict/fontys/vider/MainActivity.java
+++ Vider/app/src/main/java/org/fhict/fontys/vider/MainActivity.java
@@ -1,7 +1,11 @@
 package org.fhict.fontys.vider;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
