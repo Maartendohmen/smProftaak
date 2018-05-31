d4c76debce35bc3fc3440828df96b5ec3254faa8
Ron <ron.manders@student.fontys.nl>
2018-05-31 11:39:02 +0200

    firebase database import needed

--- Vider/app/src/main/java/org/fhict/fontys/vider/DocterChatActivity.java
+++ Vider/app/src/main/java/org/fhict/fontys/vider/DocterChatActivity.java
@@ -2,6 +2,11 @@ package org.fhict.fontys.vider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.fhict.fontys.vider.Models.ChatMessage;
 
 public class DocterChatActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class DocterChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_docter_chat);
+
+
+        Button sendButton = findViewById(R.id.sendMessageImagebutton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.messageEditText);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
     }
 }
