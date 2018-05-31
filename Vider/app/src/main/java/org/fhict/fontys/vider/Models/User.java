d148eb5981c3a2661dff6aff360d642b511e9384
Rik <rik.vanspreuwel@student.fontys.nl>
2018-05-31 11:06:21 +0200

    Create Models User and Role. Created docterLIstActivity.

---
+++ Vider/app/src/main/java/org/fhict/fontys/vider/Models/User.java
@@ -0,0 +1,37 @@
+package org.fhict.fontys.vider.Models;
+
+public class User {
+    private String uid;
+    private String userName;
+    private Role role;
+
+    public User(String uid, String userName, Role role) {
+        this.uid = uid;
+        this.userName = userName;
+        this.role = role;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
