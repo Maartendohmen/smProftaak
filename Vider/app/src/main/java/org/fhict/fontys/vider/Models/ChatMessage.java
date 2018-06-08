db14a2d9daef3aa4ff916e7a451aed3e59011f8e
Unknown <m.hennen@student.fontys.nl>
2018-06-08 14:52:20 +0200

    Chat

--- Vider/app/src/main/java/org/fhict/fontys/vider/Models/ChatMessage.java
+++ Vider/app/src/main/java/org/fhict/fontys/vider/Models/ChatMessage.java
@@ -1,17 +1,22 @@
 package org.fhict.fontys.vider.Models;
 
+import android.support.annotation.NonNull;
+
+import java.util.Comparator;
 import java.util.Date;
 
 public class ChatMessage {
     private String messageText;
-    private String messageUser;
+    private String messageReceiver;
+    private String messageSender;
     private long messageTime;
 
-    public ChatMessage(String messageText, String messageUser) {
+    public ChatMessage(String messageText, String messageReceiver, String messageSender) {
         this.messageText = messageText;
-        this.messageUser = messageUser;
+        this.messageReceiver = messageReceiver;
+        this.messageSender = messageSender;
 
-        // Initialize to current time
+        //Initialize time
         messageTime = new Date().getTime();
     }
 
@@ -27,12 +32,20 @@ public class ChatMessage {
         this.messageText = messageText;
     }
 
-    public String getMessageUser() {
-        return messageUser;
+    public String getMessageReceiver() {
+        return messageReceiver;
     }
 
-    public void setMessageUser(String messageUser) {
-        this.messageUser = messageUser;
+    public void setMessageReceiver(String messageReceiver) {
+        this.messageReceiver = messageReceiver;
+    }
+
+    public String getMessageSender() {
+        return messageSender;
+    }
+
+    public void setMessageSender(String messageSender) {
+        this.messageSender = messageSender;
     }
 
     public long getMessageTime() {
@@ -42,4 +55,25 @@ public class ChatMessage {
     public void setMessageTime(long messageTime) {
         this.messageTime = messageTime;
     }
+
+    static class ChatMessageComparator implements Comparator<ChatMessage>
+    {
+
+        public int compare(ChatMessage o1, ChatMessage o2) {
+            long time1 = o1.getMessageTime();
+            long time2 = o2.getMessageTime();
+
+            if(time1 == time2){
+                return 0;
+            }
+            else if(time1 <= time2){
+                return 1;
+            }
+            else if(time2 <= time1){
+                return -1;
+            }
+            return 0;
+        }
+    }
+
 }
