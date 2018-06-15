24cc60ba38fac36708dba2cc1b4a15b00b8b95ba
Ron <ron.manders@student.fontys.nl>
2018-06-15 11:15:51 +0200

    Created clicklisteners

--- Vider/app/src/main/java/org/fhict/fontys/vider/SendComplainActivity.java
+++ Vider/app/src/main/java/org/fhict/fontys/vider/SendComplainActivity.java
@@ -2,6 +2,9 @@ package org.fhict.fontys.vider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SendComplainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class SendComplainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_complain);
+
+        EditText complain = findViewById(R.id.editTextComplain);
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        Button sendButton = findViewById(R.id.sendButton);
+
+        imageButton.setOnClickListener(v -> {
+            System.out.println("Klik imagebutton");
+            OpenfotoIntent();
+        });
+        sendButton.setOnClickListener(v -> {
+            System.out.println("Klik sendbutton");
+            SendComplain();
+        });
     }
+
+    private void OpenfotoIntent() {
+    }
+
+    private void SendComplain() {
+    }
+
+
+
 }
